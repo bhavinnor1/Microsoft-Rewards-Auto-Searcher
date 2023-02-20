b3fce407b755e7e60cec4e15192ec6d4c8ed158d
Bhavin Nor <79081893+bhavinnor1@users.noreply.github.com>
2023-02-20 21:59:14 +0530

    Create auto search.py

---
+++ auto search.py
@@ -0,0 +1,22 @@
+import pyautogui
+import random
+from time import sleep
+alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+def random_word():
+    word_length=random.randrange(1,20)
+    word=""
+    for i in range(0,word_length):
+        random_alphabet_index=random.randrange(0,25)
+        word+=alphabet[random_alphabet_index]
+    return word
+n=1
+word=""
+while n<61:
+    pyautogui.moveTo(random.randrange(300,450),random.randrange(125,130))
+    pyautogui.press("backspace",presses=len(word))
+    pyautogui.click()
+    word=random_word()
+    pyautogui.write(word,interval=(random.randrange(1,3)/10))
+    pyautogui.press("enter")
+    sleep(random.randrange(1,5))
+    n+=1
